28bf4e94622fdae0efc14aad7c8a3f52ce26aa59
sachin <sachinshinde7676@gmail.com>
2016-12-06 13:34:35 +0530

    Updated project in accordance with latest library

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,11 +3,11 @@
  */
 import com.neovisionaries.ws.client.WebSocketException;
 import com.neovisionaries.ws.client.WebSocketFrame;
-import io.github.sac.*;
+import io.github.sac.BasicListener;
+import io.github.sac.Socket;
 
 import java.util.List;
 import java.util.Map;
-import java.util.Scanner;
 
 /**
  * Created by sachin on 8/11/16.
@@ -22,37 +22,28 @@ public class Main {
         Socket socket = new Socket(url);
 
         socket.setListener(new BasicListener() {
-
-            public void onConnected(Map<String, List<String>> headers) {
-                System.out.println("Connected to endpoint");
+            public void onConnected(Socket socket, Map<String, List<String>> headers) {
+                System.out.println("Connected to server");
             }
 
-            public void onDisconnected(WebSocketFrame serverCloseFrame, WebSocketFrame clientCloseFrame, boolean closedByServer) {
-                System.out.println("Disconnected from end-point");
+            public void onDisconnected(Socket socket, WebSocketFrame serverCloseFrame, WebSocketFrame clientCloseFrame, boolean closedByServer) {
+
             }
 
-            public void onConnectError(WebSocketException exception) {
-                System.out.println("Got connect error "+ exception);
+            public void onConnectError(Socket socket, WebSocketException exception) {
+
+            }
+
+            public void onAuthentication(Socket socket, Boolean status) {
+
             }
 
             public void onSetAuthToken(String token, Socket socket) {
-                socket.setAuthToken(token);
-            }
 
-            public void onAuthentication(Boolean status) {
-                if (status) {
-                    System.out.println("socket is authenticated");
-                } else {
-                    System.out.println("Authentication is required (optional)");
-                }
             }
-
         });
 
-        socket.setReconnection(new ReconnectStrategy().setDelay(2000).setMaxAttempts(2)); //Connect after each 2 seconds for 30 times
-
         socket.connect();
-        socket.disableLogging();
 
     }
 }
