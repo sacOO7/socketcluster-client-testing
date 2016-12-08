a84492206715547c60b62c9311f6a4bc48eab44b
sachin <sachinshinde7676@gmail.com>
2016-12-09 00:28:21 +0530

    Updated methods in main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,6 +9,8 @@ import io.github.sac.Ack;
 import io.github.sac.BasicListener;
 import io.github.sac.Emitter;
 import io.github.sac.Socket;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.List;
 import java.util.Map;
@@ -19,9 +21,29 @@ import java.util.Map;
 
 public class Main {
 
+//    public void nativeKeyPressed(NativeKeyEvent e) {
+//        System.out.println("Key Pressed: " + NativeKeyEvent.getKeyText(e.getKeyCode()));
+//
+//        if (e.getKeyCode() == NativeKeyEvent.VC_ESCAPE) {
+//            try {
+//                GlobalScreen.unregisterNativeHook();
+//            } catch (NativeHookException e1) {
+//                e1.printStackTrace();
+//            }
+//        }
+//    }
+//
+//    public void nativeKeyReleased(NativeKeyEvent e) {
+//        System.out.println("Key Released: " + NativeKeyEvent.getKeyText(e.getKeyCode()));
+//    }
+//
+//    public void nativeKeyTyped(NativeKeyEvent e) {
+//        System.out.println("Key Typed: " + e.getKeyText(e.getKeyCode()));
+//    }
+
     public static String url="ws://localhost:8000/socketcluster/";
 
-    public static void main(String arg[]) {
+    public static void main(String arg[]) throws JSONException {
 
         Socket socket = new Socket(url);
 
@@ -56,59 +78,83 @@ public class Main {
 
         socket.connect();
 
+        socket.createChannel("Myclassroom").subscribe();
+        JSONObject object=new JSONObject();
+        object.put("message","sachin");
+        object.put("data","Hi gandu");
+        socket.getChannelByName("Myclassroom").publish(object);
+
+        socket.getChannelByName("Myclassroom").onMessage(new Emitter.Listener() {
+            @Override
+            public void call(String name, Object data) {
+                System.out.println("Got data "+data.toString());
+            }
+        });
+
 //        socket.emit("chat","Hi");
 
-        socket.emit("chat", "Hi", new Ack() {
-            @Override
-            public void call(String eventName, Object error, Object data) {
-                System.out.println("Got message for :"+eventName+" error is :"+error+" data is :"+data);
-            }
-        });
-
-//        socket.on("yell", new Emitter.Listener() {
+//        socket.emit("chat", "Hi", new Ack() {
 //            @Override
-//            public void call(String eventName, Object data) {
-//                System.out.println("Got message for :"+eventName+" data is :"+data);
+//            public void call(String eventName, Object error, Object data) {
+//                System.out.println("Got message for :"+eventName+" error is :"+error+" data is :"+data);
 //            }
 //        });
-
-        socket.on("yell", new Emitter.AckListener() {
-            @Override
-            public void call(String eventName, Object data, Ack ack) {
-                System.out.println("Got message for :"+eventName+" data is :"+data);
-                //sending ack back
-
-                ack.call(eventName,"This is error","This is data");
-            }
-        });
-
-        Socket.Channel channel = socket.createChannel("yell");
 //
-        channel.subscribe(new Ack() {
-            @Override
-            public void call(String channelName, Object error, Object data) {
-                if (error==null){
-                    System.out.println("Subscribed to channel "+channelName+" successfully");
-                }
-            }
-        });
-
-        channel.publish("Hi sachin", new Ack() {
-            @Override
-            public void call(String channelName, Object error, Object data) {
-                if (error==null){
-                    System.out.println("Published message to channel "+channelName+" successfully");
-                }
-            }
-        });
-
-        channel.onMessage(new Emitter.Listener() {
-            @Override
-            public void call(String channelName, Object data) {
-
-                System.out.println("Got message for channel "+channelName+" data is "+data);
-            }
-        });
-
+////        socket.on("yell", new Emitter.Listener() {
+////            @Override
+////            public void call(String eventName, Object data) {
+////                System.out.println("Got message for :"+eventName+" data is :"+data);
+////            }
+////        });
+//
+//        socket.on("yell", new Emitter.AckListener() {
+//            @Override
+//            public void call(String eventName, Object data, Ack ack) {
+//                System.out.println("Got message for :"+eventName+" data is :"+data);
+//                //sending ack back
+//
+//                ack.call(eventName,"This is error","This is data");
+//            }
+//        });
+//
+//        Socket.Channel channel = socket.createChannel("yell");
+////
+//        channel.subscribe(new Ack() {
+//            @Override
+//            public void call(String channelName, Object error, Object data) {
+//                if (error==null){
+//                    System.out.println("Subscribed to channel "+channelName+" successfully");
+//                }
+//            }
+//        });
+//
+//        channel.publish("Hi sachin", new Ack() {
+//            @Override
+//            public void call(String channelName, Object error, Object data) {
+//                if (error==null){
+//                    System.out.println("Published message to channel "+channelName+" successfully");
+//                }
+//            }
+//        });
+//
+//        channel.onMessage(new Emitter.Listener() {
+//            @Override
+//            public void call(String channelName, Object data) {
+//
+//                System.out.println("Got message for channel "+channelName+" data is "+data);
+//            }
+//        });
+//        try {
+//            GlobalScreen.registerNativeHook();
+//        }
+//        catch (NativeHookException ex) {
+//            System.err.println("There was a problem registering the native hook.");
+//            System.err.println(ex.getMessage());
+//
+//            System.exit(1);
+//        }
+//
+//        GlobalScreen.addNativeKeyListener(new Main());
     }
+
 }
